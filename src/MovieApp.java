dbee64d4aa1d2649bf47d0532ab3991329c480ef
Resul Huseynli <resulhuseynli4@gmail.com>
2025-03-28 18:18:51 +0400

    All Updated!

--- src/MovieApp.java
+++ src/MovieApp.java
@@ -1,44 +1,70 @@
 import java.util.Scanner;
 
-public class Main {
+public class MovieApp {
     public static Scanner scanner = new Scanner(System.in);
     public static Movie[] movies = new Movie[0];
 
     public static void main(String[] args) {
 
         System.out.println();
-        System.out.println("*********************** Welcome to the Movie App! ***********************");
+        System.out.println("******************************** Welcome to the Movie App! ********************************");
         createMenu();
     }
 
     private static void createMenu() {
 
+        loop:
         while (true) {
             displayChoices();
             String choice = scanner.nextLine();
 
-            if (choice.equals("1")) {
-                addMovie();
-            } else if (choice.equals("2")) {
-                displayMovies();
-            } else if (choice.equals("3")) {
-                displayStatistics();
-            } else if (choice.equals("4")) {
-                searchMovie();
-            } else if (choice.equals("5")) {
-                changeRatingOfMovie();
-            } else if (choice.equals("6")) {
-                deleteMovie();
-            } else if (choice.equals("7")) {
-                sortMovies();
-            } else if (choice.equals("0")) {
-                System.out.println("Goodbye!");
-                break;
-            } else {
-                System.out.println("Invalid choice. Please try again.");
+            switch (choice) {
+                case "1":
+                    addMovie(3);
+                    break;
+                case "2":
+                    addMovie(askForNumbers());
+                    break;
+                case "3":
+                    displayMovies();
+                    break;
+                case "4":
+                    displayStatistics();
+                    break;
+                case "5":
+                    searchMovie();
+                    break;
+                case "6":
+                    changeRatingOfMovie();
+                    break;
+                case "7":
+                    deleteMovie();
+                    break;
+                case "8":
+                    sortMovies();
+                    break;
+                case "0":
+                    System.out.println("******************************** Thank You For Using Our App! ********************************");
+                    break loop;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
             }
         }
+    }
 
+    private static void displayChoices() {
+        System.out.println();
+        System.out.println("1. Add three movies");
+        System.out.println("2. Add more movies");
+        System.out.println("3. Display movies");
+        System.out.println("4. Show the statistics");
+        System.out.println("5. Search for a movie");
+        System.out.println("6. Change the rating of a movie");
+        System.out.println("7. Delete the movie");
+        System.out.println("8. Sort the movies");
+        System.out.println("0. Exit");
+        System.out.println();
     }
 
     private static void sortMovies() {
@@ -91,7 +117,6 @@ public class Main {
                 return;
             }
         }
-
         System.out.println("Movie not found!");
     }
 
@@ -99,13 +124,16 @@ public class Main {
         System.out.println("Enter the movie name you want to delete");
         String name = scanner.nextLine();
 
-        Movie[] newMovies = new Movie[movies.length - 1];
+        while (!isAvailable(name)) {
+            System.out.println("Movie not found. Please enter another movie name");
+            name = scanner.nextLine();
+        }
 
+        Movie[] newMovies = new Movie[movies.length - 1];
         int i = 0;
+
         for (Movie movie : movies) {
-            if (movie.getName().equalsIgnoreCase(name)) {
-                continue;
-            } else {
+            if (!movie.getName().equalsIgnoreCase(name)) {
                 newMovies[i] = movie;
                 i++;
             }
@@ -117,15 +145,18 @@ public class Main {
         System.out.println("Enter the name of the movie you want to search for:");
         String name = scanner.nextLine();
 
+        while (!isAvailable(name)) {
+            System.out.println("Movie not found. Please enter another movie name");
+            name = scanner.nextLine();
+        }
+
         for (Movie movie : movies) {
             if (movie.getName().equalsIgnoreCase(name)) {
                 System.out.println();
-                System.out.println(movie.getName() + ":  " + movie.getRating());
+                System.out.println(movie.getName() + ": " + movie.getRating());
                 return;
             }
         }
-
-        System.out.println("Movie not found");
     }
 
     private static void displayStatistics() {
@@ -152,64 +183,45 @@ public class Main {
         }
 
         System.out.println("The average rating of all movies is: " + sum / movies.length);
-
-    }
-
-    private static void displayChoices() {
-        System.out.println();
-        System.out.println("1. Add movies");
-        System.out.println("2. Display movies");
-        System.out.println("3. Show the statistics");
-        System.out.println("4. Search for a movie");
-        System.out.println("5. Change the rating of a movie");
-        System.out.println("6. Delete the movie");
-        System.out.println("7. Sort the movies");
-        System.out.println("0. Exit");
-        System.out.println();
     }
 
     private static void displayMovies() {
-        System.out.println("****************************************");
 
+        System.out.println("**************************************************************************");
         for (Movie movie : movies) {
             if (movie != null) {
-                System.out.println("Movie name: " + movie.getName());
-                System.out.println("Movie rating: " + movie.getRating());
-                System.out.println();
+                System.out.println(movie.getName() + ": " + movie.getRating());
             }
         }
-
         if (movies.length == 0) {
             System.out.println();
             System.out.println("No movies found in the app");
             System.out.println();
         }
-
-        System.out.println("****************************************");
+        System.out.println("**************************************************************************");
     }
 
-    private static void addMovie() {
-
-        System.out.println("Enter a number of movies you want to add");
-        int numberOfMovies = scanner.nextInt();
-        scanner.nextLine();
+    private static void addMovie(int numberOfMovies) {
 
         Movie[] newMovies = new Movie[movies.length + numberOfMovies];
 
         for (int i = 0; i < movies.length; i++) {
             newMovies[i] = movies[i];
         }
-
         movies = newMovies;
 
         for (int i = 0; i < numberOfMovies; i++) {
             System.out.println("Enter a movie name");
             String name = scanner.nextLine();
 
+            while (isAvailable(name)) {
+                System.out.println("This movie is already in the list. Please enter another movie name");
+                name = scanner.nextLine();
+            }
+
             System.out.println("Enter a movie rating");
             double rating = scanner.nextDouble();
             scanner.nextLine();
-
             Movie movie = new Movie(name, rating);
 
             for (int j = 0; j < movies.length; j++) {
@@ -219,5 +231,21 @@ public class Main {
                 }
             }
         }
+    }
+
+    private static boolean isAvailable(String name) {
+        for (Movie movie : movies) {
+            if (movie != null && movie.getName().equalsIgnoreCase(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int askForNumbers() {
+        System.out.println("Enter a number of movies you want to add");
+        int numberOfMovies = scanner.nextInt();
+        scanner.nextLine();
+        return numberOfMovies;
     }
 }
